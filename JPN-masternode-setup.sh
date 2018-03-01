754ee5514b822bbafb7024468ff32a620792f5e6
liray-unendlich <unendlichkeit629@gmail.com>
2018-03-01 17:32:03 +0900

    update for masternode client install, update
    アップデート機能、新規インストール機能を付けました。

--- JPN-masternode-setup.sh
+++ JPN-masternode-setup.sh
@@ -1,29 +1,45 @@
 #!/bin/bash
 #please do this script as root.
 ######################################################################
-clear
+#オプションの判定処理
+while :
+do
+  case "$1" in
+    -v | --version)
+    version="$2" #get version data
+    shift 2
+    ;;
+    -u | --update)
+    install=0
+    update=1
+    shift
+    ;;
+    -i | --install)
+    install=1
+    update=0
+    shift
+    ;;
+    -*)
+    echo "エラー: 不明なオプションを入力しています: $1" > $2
+    exit 1
+    ;;
+    *)
+    break
+    ;;
+  esac
+done
+
 echo "*********** Phore マスターノード設定スクリプトへようこそ ***********"
 echo 'Ubuntu16.04に必要なパッケージをすべてインストールします。'
 echo 'その後Phoreのウォレットをコンパイルし、設定、実行します。'
 echo '****************************************************************************'
-sleep 3
-echo '*** ステップ 1/4 ***'
+sleep 1
 echo '*** パッケージのインストール ***'
 sleep 2
-sudo apt-get update -y
-sudo apt-get upgrade -y
-sudo apt-get dist-upgrade -y
-sudo apt-get install -y nano htop git
-sudo apt-get install -y software-properties-common
-sudo apt-get install -y build-essential libtool autotools-dev pkg-config libssl-dev libevent-dev
-sudo apt-get install -y libboost-all-dev
-sudo apt-get install -y libminiupnpc-dev
-sudo apt-get install -y autoconf
-sudo apt-get install -y automake
-sudo add-apt-repository ppa:bitcoin/bitcoin -y
-sudo apt-get update -y
-sudo apt-get dist-upgrade -y
-sudo apt-get install -y libdb4.8-dev libdb4.8++-dev
+apt-get update -y
+apt-get upgrade -y
+apt-get dist-upgrade -y
+apt-get install -y nano htop wget
 sleep 1
 echo '*** 完了 1/4 ***'
 sleep 1
@@ -47,29 +63,31 @@ phore-cli stop
 mkdir PHORE_`date '+%Y%m%d'`
 sudo mv /usr/local/bin/phored /usr/local/bin/phore-cli /usr/local/bin/phore-tx ~/PHORE_`date '+%Y%m%d'`
 sudo mv ~/phored ~/phore-cli ~/phore-tx ~/PHORE_`date '+%Y%m%d'`
-
 echo '***phoreウォレットのインストールを開始します。***'
-wget https://github.com/phoreproject/Phore/releases/download/v1.2.2/phore-1.2.2-x86_64-linux-gnu.tar.gz
-tar -xvzf phore-1.2.2-x86_64-linux-gnu.tar.gz
-cd phore-1.2.2/bin
+wget https://github.com/phoreproject/Phore/releases/download/v${version}/phore-${version}-x86_64-linux-gnu.tar.gz
+tar -xvzf phore-${version}-x86_64-linux-gnu.tar.gz
+cd phore-${version}/bin
 sudo mv phore* /usr/local/bin/
 cd
-sudo rm -r phore-1.2.2
-if [ $1 = "-u" ]; then
+rm phore-${version}-x86_64-linux-gnu.tar.gz
+sudo rm -r phore-${version}
+if [ $update -eq 1 ]; then
+  echo "アップデートを行います。"
   phored -daemon
   phore-cli getinfo
   echo "アップデートは完了しました。"
   echo "バージョンデータが新しくなっているかご確認ください。"
   echo "ご確認後、マスターノードをコントロールウォレットであるphore-qtから再始動させるのをお忘れなく。"
   echo "***終了***"
-elif [ $1 = "-i" ]; then
-  echo '*** ウォレットの起動・初期設定を行います。 ***'
+elif [ $install -eq 1 ]; then
+  echo '*** インストールとしてウォレットの起動・初期設定を行います。 ***'
   sleep 1
   mkdir .phore
   rpcusr=$(more /dev/urandom  | tr -d -c '[:alnum:]' | fold -w 20 | head -1)
   rpcpass=$(more /dev/urandom  | tr -d -c '[:alnum:]' | fold -w 20 | head -1)
   ipaddress=$(curl inet-ip.info)
   echo -n "マスターノードプライベートキー(ステップ2の結果)を入力もしくはペーストしてください。"
+  echo "***"
   read mngenkey
   echo -e "rpcuser=$rpcusr \nrpcpassword=$rpcpass \nrpcallowip=127.0.0.1 \nlisten=1 \nserver=1 \ndaemon=1 \nstaking=0 \nmasternode=1 \nlogtimestamps=1 \nmaxconnections=256 \nexternalip=$ipaddress \nbind=$ipaddress \nmasternodeaddr=$ipaddress:11771 \nmasternodeprivkey=$mngenkey \n" > ~/.phore/phore.conf
   echo '*** 設定が完了しましたので、ウォレットを起動して同期を開始します。 ***'
