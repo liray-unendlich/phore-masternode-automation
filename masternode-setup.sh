23e247f16df4268326c2be8e7505e95621e24c24
liray-unendlich <unendlichkeit629@gmail.com>
2018-04-12 13:33:13 +0900

    Update masternode-setup.sh

--- masternode-setup.sh
+++ masternode-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #please do this script as root.
 ######################################################################
-#check option
+#オプションの判定処理
 while :
 do
   case "$1" in
@@ -19,8 +19,12 @@ do
     update=0
     shift
     ;;
+    -g | --generate)
+    generate=1
+    shift
+    ;;
     -*)
-    echo "Error:invalid option $1" > $2
+    echo "エラー: 不明なオプションを入力しています: $1" > $2
     exit 1
     ;;
     *)
@@ -29,83 +33,99 @@ do
   esac
 done
 
+# Generate masternode private key
+function generate_privkey() {
+  mkdir -p /etc/masternodes/
+  echo -e "rpcuser=test\nrpcpassword=passtest" >> /etc/masternodes/phore_test.conf 
+  phored -daemon -conf=/etc/masternodes/phore_test.conf -datadir=/etc/masternodes >> mn.log
+  sleep 5
+  mngenkey=$(phore-cli -conf=/etc/masternodes/phore_test.conf -datadir=/etc/masternodes masternode genkey)
+  phore-cli -conf=/etc/masternodes/phore_test.conf -datadir=/etc/masternodes stop >> mn.log
+  sleep 5
+  rm -r /etc/masternodes/
+}
+ 
+# Make masternode.conf for ppl
+function create_mnconf() {
+  echo phore-MN01 $ipaddress:11771 $mngenkey TRANSACTION_ID TRANSACTION_INDEX >> tmp_masternode.conf
+  cat tmp_masternode.conf
+}  
+echo " "
 echo "*********** Welcome to the Phore (PHR) Masternode Setup Script ***********"
 echo 'This script will install all required updates & package for Ubuntu 16.04 !'
 echo 'This script will install openbazaar server client.'
 echo 'You can run this script on VPS only.'
 echo '****************************************************************************'
-sleep 1
 echo '*** Installing package ***'
-sleep 2
-apt-get update -y
-apt-get upgrade -y
-apt-get dist-upgrade -y
-apt-get install -y nano htop git wget
-sleep 1
-echo '*** Done 1/4 ***'
-sleep 1
+apt-get update -qqy
+apt-get upgrade -qqy
+apt-get dist-upgrade -qqy
+apt-get install -qqy nano htop git wget
 echo '*** Step 2/4 ***'
-echo '*** Starting & Configuring firewall ***'
-apt-get install -y ufw
-ufw allow ssh/tcp
-ufw limit ssh/tcp
-ufw allow 11771/tcp
-ufw logging on
-ufw --force enable
-ufw status
-sleep 1
-echo '*** Done 2/4 ***'
-sleep 1
-echo '***step 3/4***'
-echo '*** Stopping & Backup existing wallet to PHORE_date ***'
-phore-cli stop
-./phore-cli stop
-mkdir PHORE_`date '+%Y%m%d'`
-mv /usr/local/bin/phored /usr/local/bin/phore-cli /usr/local/bin/phore-tx ~/PHORE_`date '+%Y%m%d'`
-mv ~/phored ~/phore-cli ~/phore-tx ~/PHORE_`date '+%Y%m%d'`
-echo '***Installing Phore wallet***'
-wget https://github.com/phoreproject/Phore/releases/download/v${version}/phore-${version}-x86_64-linux-gnu.tar.gz
-tar -xvzf phore-${version}-x86_64-linux-gnu.tar.gz
+echo '*** Configuring firewall ***'
+apt-get install -qqy ufw
+ufw allow ssh/tcp >> mn.log
+ufw limit ssh/tcp >> mn.log
+ufw allow 11771/tcp >> mn.log
+ufw logging on >> mn.log
+ufw --force enable >> mn.log
+ufw status >> mn.log
+phore-cli stop &>> mn.log
+./phore-cli stop &>> mn.log
+echo '*** ステップ 3/4 ***'
+if [ -e /usr/local/bin/phored -o -e phored ]; then
+  echo '***Backup your existing phored. If you want to restore, please check PHORE_DATE ***'
+  mkdir PHORE_`date '+%Y%m%d'` >> mn.log
+  mv /usr/local/bin/phored /usr/local/bin/phore-cli /usr/local/bin/phore-tx ~/PHORE_`date '+%Y%m%d'` &>> mn.log
+  mv ~/phored ~/phore-cli ~/phore-tx ~/PHORE_`date '+%Y%m%d'` &>> mn.log
+fi
+echo '*** Step 4/4 ***'
+echo '***Installing phore wallet daemon***'
+wget -nv https://github.com/phoreproject/Phore/releases/download/v${version}/phore-${version}-x86_64-linux-gnu.tar.gz >> mn.log
+tar -xvzf phore-${version}-x86_64-linux-gnu.tar.gz >> mn.log
 cd phore-${version}/bin
 mv phore* /usr/local/bin/
 cd
-rm phore-${version}-x86_64-linux-gnu.tar.gz
+rm phore-${version}-x86_64-linux-gnu.tar.gz 
 rm -r phore-${version}
 if [ $update -eq 1 ]; then
-  echo "Start updating..."
+  echo "Updating"
   phored -daemon
   phore-cli getinfo
-  echo "Finish updating. Your phore-daemon is restarted."
-  echo "Please check your daemon version number."
-  echo "After checking, please restart masternode from phore-qt on Windows or Mac."
+  echo "Finish Updating"
+  echo "Check version data."
+  echo "After checking, please restart Phore masternode from phore-qt"
   echo "***End***"
 elif [ $install -eq 1 ]; then
-  echo '*** Start Installing... ***'
-  echo '*** Configuraring masternode ***'
-  sleep 1
+  echo '*** Install and configuring masternode settings ***'
   mkdir .phore
   rpcusr=$(more /dev/urandom  | tr -d -c '[:alnum:]' | fold -w 20 | head -1)
   rpcpass=$(more /dev/urandom  | tr -d -c '[:alnum:]' | fold -w 20 | head -1)
-  ipaddress=$(curl inet-ip.info)
-  echo "Please enter or paste masternode private key (result of masternode genkey)."
-  read mngenkey
-
-  while [ ${#mngenkey} -ne 51 ]
-  do
-    echo "The masternode private key you entered is invalid. please re-input."
+  ipaddress=$(curl -s inet-ip.info)
+  if [ $generate -eq 1 ]; then
+    generate_privkey
+  else
+    echo "Enter or paste masternode private key"
     read mngenkey
-  done
-  
+    while [ ${#mngenkey} -ne 51 ]
+    do
+      echo "Invalid masternode private key. please reinput."
+      read mngenkey
+    done
+  fi
   echo -e "rpcuser=$rpcusr\nrpcpassword=$rpcpass\nrpcallowip=127.0.0.1\nlisten=1\nserver=1\ndaemon=1\nstaking=0\nmasternode=1\nlogtimestamps=1\nmaxconnections=256\nexternalip=$ipaddress\nbind=$ipaddress\nmasternodeaddr=$ipaddress:11771\nmasternodeprivkey=$mngenkey\n" > ~/.phore/phore.conf
-  echo '*** Start syncing... ***'
-  phored -daemon
-  echo 'We will show result of "phore-cli getinfo" after 20 sec.'
-  sleep 20
+  echo '*** Start syncing ***'
+  phored -daemon &>> mn.log
+  echo 'After 10sec, I will show you the outputs of getinfo'
+  sleep 10
   phore-cli getinfo
-  sleep 2
-  echo 'After fully syncing, you can start masternode.'
-  sleep 2
+  echo 'After fully syncing, you can start phore masternode.'
+  echo 'There is example line for masternode.conf. Please copy this line and paste to your masternode.conf'
+  echo " "
+  create_mnconf
+  echo " "
+  echo 'You can check the line by entering  **cat tmp_masternode.conf** '
 else
-  echo "Invalid option: For Update: use '-u', Install: use '-i'"
-　echo "***End***"
+  echo "Invalid command, or argument. If you want to update, use '-u', to install, use '-i'."
+　echo "**END**"
 fi
